84292ba7326c0e46640c13f48eeb3544e4d31a8c
Assaf Toledo <assaf.toledo@ibm.com>
2022-11-21 12:01:12 +0200

    switch to a single uploader
    Signed-off-by: Assaf Toledo <assaf.toledo@ibm.com>

--- upload.py
+++ upload.py
@@ -7,10 +7,16 @@ import sys
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 from consts import MODEL_DIR, HF_MODEL_ID
+from consts import HF_DATASET_ID
+from utils import read_dataset
 
 model_args = {
     'repo_path_or_name': f'{MODEL_DIR}_hf',
     'repo_url': HF_MODEL_ID,
+}
+
+dataset_args = {
+    'repo_id': HF_DATASET_ID,
 }
 
 
@@ -21,7 +27,12 @@ if __name__ == '__main__':
 
     # add the auth token to the args
     model_args.update({'use_auth_token': sys.argv[1]})
+    dataset_args.update({'token': sys.argv[1]})
 
     # upload the model and the tokenizer
     for mt in [AutoModelForSequenceClassification, AutoTokenizer]:
         mt.from_pretrained(MODEL_DIR).push_to_hub(**model_args)
+
+    # upload the dataset
+    dataset = read_dataset()
+    dataset.push_to_hub(**dataset_args)
